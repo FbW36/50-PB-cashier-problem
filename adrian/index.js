22f1b34147c00317658985bacf2186b3037691a5
AdrianValdes <adrianvaldeshdz@gmail.com>
2020-11-24 12:11:10 +0100

    Another solution, also looping just one time

--- adrian/index.js
+++ adrian/index.js
@@ -1,4 +1,4 @@
-giveChange({ price: 3.75, paidAmount: 50 });
+giveChange1({ price: 3.75, paidAmount: 50 });
 
 function giveChange({ price, paidAmount }) {
   let change = paidAmount - price;
@@ -39,4 +39,40 @@ function giveChange({ price, paidAmount }) {
   /*   for (const key in result) {
     console.log(`${result[key]} x ${key}`);
   } */
+}
+
+function giveChange1({ price, paidAmount }) {
+  let change = paidAmount - price;
+  console.log(change);
+  if (change < 0) {
+    return `The paid amount is not enough`;
+  }
+  if (change === 0) {
+    return `Not need to give money back`;
+  }
+
+  dicCashier = {
+    "€500": 500,
+    "€100": 100,
+    "€50": 50,
+    "€20": 20,
+    "€10": 10,
+    "€5": 5,
+    "€1": 1,
+    "€0.50": 0.5,
+    "€0.20": 0.2,
+    "€0.10": 0.1,
+    "€0.05": 0.05,
+    "€0.02": 0.02,
+    "€0.01": 0.01,
+  };
+  for (const price in dicCashier) {
+    if (dicCashier[price] <= change) {
+      let billAmount = Math.floor(change / dicCashier[price]);
+      let temp = billAmount * dicCashier[price];
+      change -= temp;
+      change = Number(change.toFixed(2));
+      console.log(`${billAmount} x €${dicCashier[price]}`);
+    }
+  }
 }
